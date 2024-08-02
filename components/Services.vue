d016982119221d8756f653533f440fad97a93bc5
sheymantech <olaniyansheyi1704@gmail.com>
2024-08-02 18:18:45 +0100

    new

--- components/Services.vue
+++ components/Services.vue
@@ -4,7 +4,7 @@ import explore from "~/public/explore.png";
 
 <template>
   <div class="flex flex-col gap-y-7 justify-center items-center mt-14">
-    <h1 class="font-semibold text-3xl text-secondary">How To get Teepsee</h1>
+    <h1 class="font-semibold text-3xl text-secondary">How To get Teepseee</h1>
     <div class="flex gap-5 justify-center items-center">
       <div class="flex flex-col gap-y-4 bg-white rounded-md py-5 px-4">
         <img :src="explore" alt="" />
