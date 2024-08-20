9ff18d3ee01d9e105be75cd5aca19cbe4e9b10ec
sheymantech <olaniyansheyi1704@gmail.com>
2024-08-20 23:41:06 +0100

    new

--- pages/product.vue
+++ pages/product.vue
@@ -1,11 +1,18 @@
 <script setup>
+import product1 from "~/assets/product1.png";
 definePageMeta({
   layout: "custom",
 });
 </script>
 
 <template>
-  <div class="py-40">
-    <h1>olaniyan sheyi</h1>
+  <div
+    class="mt-20 w-full flex text-secondary gap-5 justify-center items-center px-4 sm:px-8"
+  >
+    <div
+      class="bg-white flex justify-center items-center rounded-lg h-[500px] w-full md:w-[48%]"
+    >
+      <img :src="product1" alt="" />
+    </div>
   </div>
 </template>
