86ff38c6291725e204bec417fdae22d11afe5cc1
sheymantech <olaniyansheyi1704@gmail.com>
2024-08-21 20:42:16 +0100

    new

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -12,6 +12,7 @@ definePageMeta({
   <div class="mb-10 px-4 sm:px-8 text-secondary tracking-wider">
     <h1 class="my-5 text-2xl">Cart</h1>
     <div class="flex flex-wrap justify-center items-start w-full gap-5 mb-5">
+      <!-- for tablet and desktop -->
       <div
         class="rounded-xl bg-white sm:w-full lg:w-[60%] px-4 sm:block hidden flex-col gap-y-4 pb-8"
       >
@@ -103,6 +104,93 @@ definePageMeta({
           </div>
           <div>
             <img class="w-[1rem] me-4" :src="cancel" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <!-- for mobile only -->
+      <div
+        class="sm:hidden bg-white rounded-xl p-6 flex flex-col gap-y-5 w-full"
+      >
+        <div class="flex gap-x-4 pb-5 w-full border-b-2 border-b-[#e6e3e3]">
+          <div
+            class="bg-[#e6e3e3] flex justify-center items-center h-[120px] w-[40%] rounded-lg"
+          >
+            <img :src="hennessey2" alt="" class="w-[80%] h-[90%]" />
+          </div>
+
+          <div
+            class="flex flex-col justify-start items-start font-semibold w-full"
+          >
+            <h1>Champagne Montevideo</h1>
+            <p class="text-md font-semibold">#5,000</p>
+            <p>Qty: 0</p>
+            <div class="mt-3">
+              <button
+                class="ps-4 pe-5 border-[1px] rounded-s-md border-[#666666]"
+              >
+                -
+              </button>
+              <button
+                class="ps-4 pe-5 border-[1px] rounded-e-md border-[#666666] border-s-0"
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="flex gap-x-4 pb-5 w-full border-b-2 border-b-[#e6e3e3]">
+          <div
+            class="bg-[#e6e3e3] flex justify-center items-center h-[120px] w-[40%] rounded-lg"
+          >
+            <img :src="hennessey" alt="" class="w-[80%] h-[90%]" />
+          </div>
+
+          <div
+            class="flex flex-col justify-start items-start font-semibold w-full"
+          >
+            <h1>Champagne Montevideo</h1>
+            <p class="text-md font-semibold">#5,000</p>
+            <p>Qty: 0</p>
+            <div class="mt-3">
+              <button
+                class="ps-4 pe-5 border-[1px] rounded-s-md border-[#666666]"
+              >
+                -
+              </button>
+              <button
+                class="ps-4 pe-5 border-[1px] rounded-e-md border-[#666666] border-s-0"
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="flex gap-x-4 pb-5 w-full border-b-2 border-b-[#e6e3e3]">
+          <div
+            class="bg-[#e6e3e3] flex justify-center items-center h-[120px] w-[40%] rounded-lg"
+          >
+            <img :src="hennessey1" alt="" class="w-[80%] h-[90%]" />
+          </div>
+
+          <div
+            class="flex flex-col justify-start items-start font-semibold w-full"
+          >
+            <h1>Champagne Montevideo</h1>
+            <p class="text-md font-semibold">#5,000</p>
+            <p>Qty: 0</p>
+            <div class="mt-3">
+              <button
+                class="ps-4 pe-5 border-[1px] rounded-s-md border-[#666666]"
+              >
+                -
+              </button>
+              <button
+                class="ps-4 pe-5 border-[1px] rounded-e-md border-[#666666] border-s-0"
+              >
+                +
+              </button>
+            </div>
           </div>
         </div>
       </div>
